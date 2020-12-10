4820ae6f4c07bb3171725df53171b68c37f6844f
mdtsoy <mdtsoy@yandex.ru>
2020-12-10 12:19:10 +0500

    /users/

--- internal/app/apiserver/server_test.go
+++ internal/app/apiserver/server_test.go
@@ -105,3 +105,33 @@ func TestServer_UserCreate(t *testing.T) {
 		}
 	}
 }
+
+//TestServer_NotExactURL expects request to /users to be redirected to /users/
+func TestServer_NotExactURL(t *testing.T) {
+	testCases := []struct {
+		expectedCode int
+		data         map[string]interface{}
+		response     map[string]interface{}
+	}{
+		{
+			expectedCode: 307,
+		}}
+
+	srv := apiserver.TestServer(t)
+
+	for _, tc := range testCases {
+		w := httptest.NewRecorder()
+		reqJson, err := json.Marshal(tc.data)
+		if err != nil {
+			panic(err)
+		}
+
+		req, _ := http.NewRequest("POST", "/users", bytes.NewReader(reqJson))
+		req.Header.Add("Content-Type", "application/json") // Should bind json return error otherwise
+
+		srv.ServeHTTP(w, req)
+		assert.Equal(t, tc.expectedCode, w.Code)
+		assert.Equal(t, "/users/", w.Header().Get("Location"))
+	}
+
+}
